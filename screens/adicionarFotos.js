776773e8aa32c3e1d86699697e109a5230f27ab8
Gustavo <gucostapereira777@gmail.com>
2023-09-05 11:38:33 -0300

    multipla seleção de fotos

--- screens/adicionarFotos.js
+++ screens/adicionarFotos.js
@@ -1,31 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import React, { useState } from 'react';
+import { Button, Image, View } from 'react-native';
 
 export default function ImagePickerExample() {
-  const [image, setImage] = useState(null);
+  const [images, setImages] = useState([]);
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
+  const pickImages = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
-      allowsMultipleSelection: true
+      allowsMultipleSelection: true,
     });
 
-    console.log(result);
-
     if (!result.canceled) {
-      setImage(result.assets[0].uri);
+      setImages(result.assets);
     }
   };
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button title="Escolher imagens da galeria" onPress={pickImages} />
+      {images.map((image, index) => (
+        <Image key={index} source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />
+      ))}
     </View>
   );
 }
